933309608518e70bdf71885e93f9c196110cf7d5
giuse <giuseleo11@gmail.com>
2020-09-28 11:00:01 +0200

    creazione bclassification, creazione piccole funzioni e formattazione output KNN

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import master
 import analysis
 import DataVisualization
+import bclassification
 
 def main():
 
-    DataVisualization.performDataVis()
+    #DataVisualization.performDataVis()
 
 
-    #df = master.init()
+    df = master.init()
     #print(df)
 
-    #X, y = master.preproc(df)
+    X, y = master.preproc(df)
 
     #analysis.preliminaryStat(X, df['binary'])
 
     #analysis.heatMap(X,y)
-    #master.kNN(X,y)
 
-    #X_num= master.select_numerical(X)
+    bclassification.kNN(X, y, search=True, cv=False)
 
-    #master.kNN(X_num,y)
+    #bclassification.kNN(X,y,onlynum=True, search=True, cv=False)
+
+    #bclassification.kNN(X, y)
+
+    #bclassification.kNN(X, y, onlycv=True)
+
+    #bclassification.kNN(X,y,onlynum=True)
 
     #metto prima lda che fa un po' schifo
     #master.LDA(X,y)
